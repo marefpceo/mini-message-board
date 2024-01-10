6976f9d03552e46beb0e19eb7a4e85c64c230dad
marefpceo <32721274+marefpceo@users.noreply.github.com>
2024-01-10 08:28:28 -0800

    Changed user to new in routes folder and updated
    Added message array for sample messages to index route. Created template
    for index to display messages.
    
    Updated app.js to account for name change of user route to new

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,23 @@
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date()
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date()
+  }
+]
+
 const express = require('express');
 
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Message Board', messages: messages });
 });
 
 module.exports = router;
