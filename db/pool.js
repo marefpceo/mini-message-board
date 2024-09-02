118e25ccaf8bd9871e632f0dcf9138bef83f82bf
marefpceo <32721274+marefpceo@users.noreply.github.com>
2024-09-01 17:34:03 -0700

    Edited connection

--- db/pool.js
+++ db/pool.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 module.exports = new Pool({
-  connectionString: process.env.POSTGRESQL_URI || "postgresql://marefpceo:***REMOVED***@localhost:5432/message_board"
+  connectionString: process.env.POSTGRESQL_URI
 });
